293759f607eee0dc88419eeda9e660da1641badc
sarixxxxx <s.gonzalezd@uniandes.edu.co>
2026-03-09 10:52:59 -0500

    librerías de tests del demonio las odio con mi vida

---
+++ parcial1-web/__tests__/crear.test.tsx
@@ -0,0 +1,31 @@
+import {render,screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import CrearAuthor from '@/app/crear/page';
+import test, { describe} from 'node:test';
+
+const setup= () =>{
+    const user = userEvent.setup();
+    render(<CrearAuthor />);
+
+    const nameInput = screen.getByLabelText(/nombre/i) as HTMLInputElement;
+    const birthDateInput = screen.getByLabelText(/fecha de nacimiento/i) as HTMLInputElement;
+    const descriptionInput = screen.getByLabelText(/descripción/i) as HTMLInputElement;
+    const imageInput = screen.getByLabelText(/imagen (url)/i) as HTMLInputElement;
+
+    const saveBtn =screen.getByRole('button', {name: /guardar/i});
+
+    return {user,nameInput, birthDateInput, descriptionInput,imageInput, saveBtn}
+    
+
+}
+
+describe('Render de /crear', () =>{
+    test('renderiza heading, campos y ayuda; botón deshabilitado al inicio', () =>{
+        const {saveBtn} =setup();
+        expect(screen.getByLabelText(/nombre/i)).toBeInDocument();
+        expect(saveBtn).toBeDisabled();
+
+    }
+
+    )
+})
